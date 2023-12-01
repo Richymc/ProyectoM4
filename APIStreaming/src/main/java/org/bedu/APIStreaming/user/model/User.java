1130902a50aee19762cd089c66cf792e1d820c36
Alan-Coronado <alancrd16@gmail.com>
2023-11-30 21:29:04 -0700

    add user id

--- APIStreaming/src/main/java/org/bedu/APIStreaming/user/model/User.java
+++ APIStreaming/src/main/java/org/bedu/APIStreaming/user/model/User.java
@@ -1,14 +1,25 @@
 package org.bedu.APIStreaming.user.model;
 
 public class User {
+
+    private long id;
     private String lastName;
     private String fistName;
     private String email;
 
-    public User(String lastName, String fistName, String email) {
+    public User( long id, String lastName, String fistName, String email) {
+        this.id = id;
         this.lastName = lastName;
         this.fistName = fistName;
         this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getLastName() {
