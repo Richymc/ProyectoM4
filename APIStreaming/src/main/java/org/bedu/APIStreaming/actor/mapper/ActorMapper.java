5174080b90972e534577cbb8b591b91556a2b137
Alan-Coronado <alancrd16@gmail.com>
2023-12-07 12:06:24 -0700

    add mapper actor

---
+++ APIStreaming/src/main/java/org/bedu/APIStreaming/actor/mapper/ActorMapper.java
@@ -0,0 +1,21 @@
+package org.bedu.APIStreaming.actor.mapper;
+
+import org.bedu.APIStreaming.actor.dto.ActorDTO;
+import org.bedu.APIStreaming.actor.dto.CreateActorDTO;
+import org.bedu.APIStreaming.actor.model.Actor;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface ActorMapper {
+
+    ActorDTO toDTO(Actor model);
+    List<ActorDTO> toDTO(List<Actor> model);
+
+    @Mapping(target = "id", ignore = true)
+    Actor toModel(CreateActorDTO dto);
+
+}
