d5ae581667c734bbdebec5525fb3d47aa1a96165
Alan-Coronado <alancrd16@gmail.com>
2023-12-07 11:59:27 -0700

    add actor dto

---
+++ APIStreaming/src/main/java/org/bedu/APIStreaming/actor/dto/ActorDTO.java
@@ -0,0 +1,10 @@
+package org.bedu.APIStreaming.actor.dto;
+
+import lombok.Data;
+
+@Data
+public class ActorDTO {
+    private long id;
+    private String firstName;
+    private String lastName;
+}
