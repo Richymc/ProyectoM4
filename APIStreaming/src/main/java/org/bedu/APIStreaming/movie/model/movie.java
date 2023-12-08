48189809bb827a806003b568e72282399b0a1bd9
RichyCordova <ricardo.montoya.c@gmail.com>
2023-12-08 08:32:26 -0800

    add model movie

---
+++ APIStreaming/src/main/java/org/bedu/APIStreaming/movie/model/movie.java
@@ -0,0 +1,20 @@
+package org.bedu.APIStreaming.movie.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class movie {
+
+    private int id;
+
+    private String nameMovie;
+
+    private Long duration;
+
+    private String description;
+
+    private String genre;
+}
