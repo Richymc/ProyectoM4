4adeaf3f560a17475ea128f3bdb776f740a6df28
RichyCordova <ricardo.montoya.c@gmail.com>
2023-12-08 11:12:03 -0800

    Added MobileMapper and dependency injection in movie service

--- APIStreaming/src/main/java/org/bedu/APIStreaming/movie/service/MovieService.java
+++ APIStreaming/src/main/java/org/bedu/APIStreaming/movie/service/MovieService.java
@@ -1,6 +1,9 @@
 package org.bedu.APIStreaming.movie.service;
 
+import org.bedu.APIStreaming.movie.dto.MovieDTO;
+import org.bedu.APIStreaming.movie.mapper.MovieMapper;
 import org.bedu.APIStreaming.movie.repository.MovieRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +13,10 @@ public class MovieService {
     @Autowired
     private MovieRepository repository;
 
+    @Autowired
+    private MovieMapper mapper;
+
+    public List<MovieDTO> findAll(){
+        return repository.findAll().stream().map(mapper::toDTO).toList();
+    }
 }
