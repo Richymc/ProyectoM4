e5f04326974e465c4a47e2b561cbd8c686c3deec
Alan Coronado <alancrd16@gmail.com>
2024-02-07 11:28:21 -0700

    create DirectorRepositoryTest

---
+++ src/test/java/org/bedu/repository/DirectorRepositoryTest.java
@@ -0,0 +1,18 @@
+package org.bedu.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class DirectorRepositoryTest {
+
+
+}
