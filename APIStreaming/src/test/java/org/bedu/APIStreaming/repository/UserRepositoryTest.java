8aa4d4c8dddda7c5565a595d9f9ab1da89891b77
Alan Coronado <alancrd16@gmail.com>
2024-02-06 21:40:14 -0700

    add user repository test

--- APIStreaming/src/test/java/org/bedu/APIStreaming/repository/UserRepositoryTest.java
+++ APIStreaming/src/test/java/org/bedu/APIStreaming/repository/UserRepositoryTest.java
@@ -1,4 +1,26 @@
 package org.bedu.APIStreaming.repository;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    @DisplayName("UserRepository should be injected")
+    void smokeTest(){
+        assertNotNull(repository);
+    }
+
 }
