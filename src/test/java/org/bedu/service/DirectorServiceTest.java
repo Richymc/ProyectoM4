b391fb0c0e06c9ae3314e04555cdf0b9e08c9eef
Alan Coronado <alancrd16@gmail.com>
2024-02-06 23:03:40 -0700

    create DirectorServiceTest file

---
+++ src/test/java/org/bedu/service/DirectorServiceTest.java
@@ -0,0 +1,10 @@
+package org.bedu.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+public class DirectorServiceTest {
+}
