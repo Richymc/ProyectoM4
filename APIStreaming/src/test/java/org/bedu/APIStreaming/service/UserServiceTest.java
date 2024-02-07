a97c599143400a7005c93ad2ab5dc0762e77a1e0
Alan Coronado <alancrd16@gmail.com>
2024-02-06 21:48:06 -0700

    setting file user service test

--- APIStreaming/src/test/java/org/bedu/APIStreaming/service/UserServiceTest.java
+++ APIStreaming/src/test/java/org/bedu/APIStreaming/service/UserServiceTest.java
@@ -1,4 +1,20 @@
 package org.bedu.APIStreaming.service;
 
+import org.bedu.APIStreaming.repository.UserRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class UserServiceTest {
+
+    @MockBean
+    private UserRepository repository;
+
+    @Autowired
+    private UserService service;
+
 }
