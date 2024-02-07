87bc9266bbac5fa57fdc08b69c997dfff6d910ae
Alan Coronado <alancrd16@gmail.com>
2024-02-07 11:25:02 -0700

    add test repository should be injected

--- src/test/java/org/bedu/repository/ProfileRepositoryTest.java
+++ src/test/java/org/bedu/repository/ProfileRepositoryTest.java
@@ -1,13 +1,25 @@
 package org.bedu.repository;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ProfileRepositoryTest {
 
+    @Autowired
+    private ProfileRepository repository;
+
+    @Test
+    @DisplayName("ProfileRepository should be injected")
+    void smokeTest(){
+        assertNotNull(repository);
+    }
 }
