e43b7641ba003e59b961792540ecd6684a5bb247
Alan Coronado <alancrd16@gmail.com>
2024-02-06 23:19:48 -0700

    add test save director to repository

--- src/test/java/org/bedu/service/DirectorServiceTest.java
+++ src/test/java/org/bedu/service/DirectorServiceTest.java
@@ -1,7 +1,11 @@
 package org.bedu.service;
 
+import org.bedu.dto.CreateDirectorDTO;
+import org.bedu.dto.CreateUserDTO;
 import org.bedu.dto.DirectorDTO;
+import org.bedu.dto.UserDTO;
 import org.bedu.model.Director;
+import org.bedu.model.User;
 import org.bedu.repository.DirectorRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +19,7 @@ import java.util.LinkedList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -51,6 +56,26 @@ public class DirectorServiceTest {
         assertEquals(director.getId(), result.get(0).getId());
         assertEquals(director.getFirstName(), result.get(0).getFirstName());
         assertEquals(director.getLastName(), result.get(0).getLastName());
+    }
+
+    @Test
+    @DisplayName("DirectorService should save a director in repository")
+    void saveTest(){
+        CreateDirectorDTO dto = new CreateDirectorDTO();
+        dto.setFirstName("Woody");
+        dto.setLastName("Allen");
+        Director director = new Director();
+        director.setId(55);
+        director.setFirstName(dto.getFirstName());
+        director.setLastName(dto.getLastName());
+
+        when(repository.save(any(Director.class))).thenReturn(director);
+        DirectorDTO result = service.save(dto);
+
+        assertNotNull(result);
+        assertEquals(director.getId(), result.getId());
+        assertEquals(director.getFirstName(), result.getFirstName());
+        assertEquals(director.getLastName(), result.getLastName());
     }
 
 
