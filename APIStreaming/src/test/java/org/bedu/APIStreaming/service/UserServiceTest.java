d9f2c6743a69940dbcb76f1d15a8cfc73750ce6a
Alan Coronado <alancrd16@gmail.com>
2024-02-06 21:49:06 -0700

    add test userservice should be injected

--- APIStreaming/src/test/java/org/bedu/APIStreaming/service/UserServiceTest.java
+++ APIStreaming/src/test/java/org/bedu/APIStreaming/service/UserServiceTest.java
@@ -1,11 +1,15 @@
 package org.bedu.APIStreaming.service;
 
 import org.bedu.APIStreaming.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
@@ -16,5 +20,11 @@ public class UserServiceTest {
 
     @Autowired
     private UserService service;
+
+    @Test
+    @DisplayName("UserService should be injected")
+    void smokeTest(){
+        assertNotNull(service);
+    }
 
 }
