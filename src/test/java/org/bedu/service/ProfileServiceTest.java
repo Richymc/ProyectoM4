9e16a723ccac2c35f0a766275143d452ca3489fc
Alan Coronado <alancrd16@gmail.com>
2024-02-06 23:48:31 -0700

    add find test

--- src/test/java/org/bedu/service/ProfileServiceTest.java
+++ src/test/java/org/bedu/service/ProfileServiceTest.java
@@ -1,5 +1,7 @@
 package org.bedu.service;
 
+import org.bedu.dto.ProfileDTO;
+import org.bedu.model.Profile;
 import org.bedu.repository.ProfileRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,8 +10,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
+import java.util.LinkedList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
@@ -25,6 +31,24 @@ public class ProfileServiceTest {
     @DisplayName("ProfileService should be injected")
     void smokeTest(){
         assertNotNull(service);
+    }
+
+    @Test
+    @DisplayName("ProfileService should return profiles from repository")
+    void findAll(){
+        List<Profile> data = new LinkedList();
+        Profile profile = new Profile();
+        profile.setId(40);
+        profile.setName("Panchito");
+        data.add(profile);
+
+        when(repository.findAll()).thenReturn(data);
+        List<ProfileDTO> result = service.findAll();
+
+        assertNotNull(result);
+        assertTrue(result.size() > 0);
+        assertEquals(profile.getId(), result.get(0).getId());
+        assertEquals(profile.getName(), result.get(0).getName());
     }
 
 }
