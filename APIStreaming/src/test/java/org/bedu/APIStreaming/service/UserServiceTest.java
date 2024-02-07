0c35e69ba646d3adda1af1b01dacdda28801c8b8
Alan Coronado <alancrd16@gmail.com>
2024-02-06 21:52:16 -0700

    add test userService should return users from repository

--- APIStreaming/src/test/java/org/bedu/APIStreaming/service/UserServiceTest.java
+++ APIStreaming/src/test/java/org/bedu/APIStreaming/service/UserServiceTest.java
@@ -1,5 +1,8 @@
 package org.bedu.APIStreaming.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.bedu.APIStreaming.dto.UserDTO;
+import org.bedu.APIStreaming.model.User;
 import org.bedu.APIStreaming.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,8 +11,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
+import java.util.LinkedList;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
@@ -25,6 +31,30 @@ public class UserServiceTest {
     @DisplayName("UserService should be injected")
     void smokeTest(){
         assertNotNull(service);
+    }
+
+    @Test
+    @DisplayName("UserService should return users from repository")
+    void findAll(){
+        List<User> data = new LinkedList();
+
+        User user = new User();
+
+        user.setId(40);
+        user.setFirstName("Raul");
+        user.setLastName("Jimenez");
+
+        data.add(user);
+
+        when(repository.findAll()).thenReturn(data);
+
+        List<UserDTO> result = service.findAll();
+
+        assertNotNull(result);
+        assertTrue(result.size() > 0);
+        assertEquals(user.getId(), result.get(0).getId());
+        assertEquals(user.getFirstName(), result.get(0).getFirstName());
+        assertEquals(user.getLastName(), result.get(0).getLastName());
     }
 
 }
